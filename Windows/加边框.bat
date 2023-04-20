e05e3c6bd5bef538581d2000f2fdd60d552c2cb7
liaoxy <liaoxiangyun@gmail.com>
2023-04-20 16:51:59 +0800

    .

--- Windows/加边框.bat
+++ Windows/加边框.bat
@@ -6,5 +6,5 @@ echo %1
 rem 获取外部第二个参数
 echo %2
 rem 路径改为当前项目的绝对路径
-node "D:\Projects\2022-11\image-bordering\index.js" %1 %2 %3 %4 %5 %6 %7 %8 %9
+node "..\index.js" %1 %2 %3 %4 %5 %6 %7 %8 %9
 rem Pause
